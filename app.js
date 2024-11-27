66becf34fa7f02e30ed0a3d31b2b3c1bcfd8d640
Muhammad-Unaid <unaidsaab@gmail.com>
2024-11-27 13:11:22 +0500

    working

--- app.js
+++ app.js
@@ -1,77 +1,104 @@
 let boxes = document.querySelectorAll(".box");
-let reset = document.querySelector("#reset");
-let newGame = document.querySelector("#new-game");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg")
+let msg = document.querySelector("#msg");
 
-var trun0 = true;    //playerX , playerO
+let turnO = true; //playerX, playerO
+let count = 0; //To Track Draw
 
-const winPatterns =[
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,3,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8],
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
-boxes.forEach((box) => {
-    box.addEventListener("click" , () => {
-        console.log("box was click")
-        if( trun0){
-            box.innerText = "O"
-            trun0 = false;
-        }else {
-            box.innerText = "X"
-            trun0 = true;
-        }
-        box.disabled = true;
 
-        checkWinner();
-    });
+const resetGame = () => {
+  turnO = true;
+  count = 0;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+};
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      //playerO
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      //playerX
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    count++;
+
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
+  });
 });
 
-const showWinner = (Winner) => {
-    msg.innerText = `Congratulation , Winner is ${Winner}`;
-    msgContainer.classList.remove("hide");
-    disabledbox();
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
 
-}
+const disableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = true;
+  }
+};
+
+const enableBoxes = () => {
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
+};
+
+const showWinner = (winner) => {
+  msg.innerText = `Congratulations 🎉🥳 Winner is ${winner}`;
+  Swal.fire({
+    title:  `Congratulation  "${winner}"  is Winner  `,
+    width: 500,
+    padding: "3em",
+    color: "#ffff",
+    background: "#fff url(https://img.freepik.com/free-vector/ribbon-cutting-ceremony-poster-with-red-curtains-vector-illustration_91128-938.jpg) ",
+    backdrop: `
+      rgba(0,0,8,0.4)
+      url("https://media.tenor.com/3vO0Yht3MEUAAAAj/congratulation-hbd.gif")
+      left top
+      no-repeat
+    `
+  });
+  msgContainer.classList.remove("hide");
+  disableBoxes();
+};
 
 const checkWinner = () => {
-    for( var patten of winPatterns){
-        
-        var pos1Val = boxes[patten[0]].innerText;
-        var pos2Val = boxes[patten[1]].innerText;
-        var pos3Val = boxes[patten[2]].innerText;
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" &&  pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                console.log("Winner" , pos1Val);
-                showWinner(pos1Val);
-            }
-        }
-        
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return true;
+      }
     }
+  }
 };
 
-const disabledbox = () => {
-    for (var box of boxes){
-        box.disabled = true;
-    }
-};
-const enablebox = () => {
-    for (var box of boxes){
-        box.disabled = true;
-        box.innerText = "";
-    }
-};
-const resetGame = () => {
-    trun0 = true;
-    enablebox();
-    msgContainer.classList.add("hide");
-};
-
-newGame.addEventListener("click", resetGame);
-reset.addEventListener("click", resetGame)
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
